9f74a366556757ddce846c8d509043318d07c45e
Lucius Holzinger <luciholzinger@gmail.com>
2024-12-03 18:20:32 -0800

    Fixed the styling for main page and login page for ui consistency

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -31,32 +31,57 @@ export default function Login() {
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <input
-        type="email"
-        value={credentials.email}
-        onChange={(e) =>
-          setCredentials({
-            ...credentials,
-            email: e.target.value,
-          })
-        }
-        placeholder="Email"
-        required
-      />
-      <input
-        type="password"
-        value={credentials.password}
-        onChange={(e) =>
-          setCredentials({
-            ...credentials,
-            password: e.target.value,
-          })
-        }
-        placeholder="Password"
-        required
-      />
-      <button type="submit">Login</button>
-    </form>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+        <form onSubmit={handleLogin} className="max-w-md mx-auto">
+          <div className="mb-4">
+            <label htmlFor="email" className="block mb-2">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={credentials.email}
+              onChange={(e) =>
+                setCredentials({
+                  ...credentials,
+                  email: e.target.value,
+                })
+              }
+              placeholder="Email"
+              required
+              className="w-full px-3 py-2 border rounded"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="password" className="block mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              value={credentials.password}
+              onChange={(e) =>
+                setCredentials({
+                  ...credentials,
+                  password: e.target.value,
+                })
+              }
+              className="w-full px-3 py-2 border rounded"
+              placeholder="Password"
+              required
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,5 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 
 export default function Home() {
-  return <>Hello world</>;
+  return <h2 className="text-2xl font-bold mb-6 text-center">Hello world</h2>;
 }
