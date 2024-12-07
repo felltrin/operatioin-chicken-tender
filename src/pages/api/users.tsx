b079a9614a816921a27e4b668e2f1d19f7711043
Lucius Holzinger <luciholzinger@gmail.com>
2024-12-07 11:12:13 -0800

    I removed the await keyword from the client because it is redundant

--- src/pages/api/users.tsx
+++ src/pages/api/users.tsx
@@ -4,8 +4,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   return new Promise(async (resolve, reject) => {
     try {
-      const client = await clientPromise;
+      // const client = await clientPromise;
       // const db = client.db("give_me_loot");
+      const client = clientPromise;
       const db = client.db("test");
       const users = await db
         .collection("users")
