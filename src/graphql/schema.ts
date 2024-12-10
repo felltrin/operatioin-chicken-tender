95456e5ec27fd807dc20691cf6a3d21df85b4011
Lucius Holzinger <luciholzinger@gmail.com>
2024-12-10 09:02:56 -0800

    Removed to be added content that needs testing. Will be back

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -8,22 +8,9 @@ export const typeDefs = `#graphql
     password: String!,
     createdAt: Date!,
   }
-
-  type AuthPayload {
-    success: Boolean!,
-    message: String,
-    token: String,
-  }
-
+  
   type Query {
     users: [User],
     user(id: ID!): User,
-    userByEmail(email: String!): User,
   }
-
-  type Mutation {
-    verifyForgotPassword(email: String!): AuthPayload!,
-    resetPassword(email: String!, token: String!, newPassword: String!): AuthPayload!
-  }
-
 `;
